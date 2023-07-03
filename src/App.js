a1a8f5dc220496ad785fb22b35e4dc3ce2ef848f
seheichenko-anna <seheichenko.anna@gmail.com>
2023-07-03 13:50:30 +0300

    Added API integration

--- src/App.js
+++ src/App.js
@@ -1,68 +1,82 @@
 import "./App.css";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
-export default function Weather() {
-  let weatherData = {
-    city: "New York",
-    temperature: 19,
-    date: "Tuesday 10:00",
-    description: "Cloudy",
-    imgUrl: "http://openweathermap.org/img/wn/50d@2x.png",
-    humidity: 80,
-    wind: 10,
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: "http://openweathermap.org/img/wn/50d@2x.png",
+      date: "",
+    });
+  }
 
-  return (
-    <div className="Weather">
-      <form id="search-form">
-        <div className="search-city-input">
-          <input
-            type="search"
-            placeholder="Enter a city..."
-            className="form-control search-input"
-            id="city-input"
-            autoComplete="off"
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form id="search-form">
+          <div className="search-city-input">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control search-input"
+              id="city-input"
+              autoComplete="off"
+            />
+          </div>
+          <div className="search-city-btn">
+            <input
+              type="submit"
+              className="btn btn-primary w-100"
+              value="Search"
+            />
+          </div>
+        </form>
+        <div className="name-current-city">
+          <h1 id="city">{weatherData.city} </h1>
+        </div>
+        <div className="current-weather">
+          <img
+            src={weatherData.iconUrl}
+            alt={weatherData.description}
+            id="icon"
+            width="100"
           />
+          <p id="temperature">{Math.round(weatherData.temperature)}</p>
+          <div className="units">
+            <sup>°C</sup>
+          </div>
         </div>
-        <div className="search-city-btn">
-          <input
-            type="submit"
-            className="btn btn-primary w-100"
-            value="Search"
-          />
-        </div>
-      </form>
-      <div className="name-current-city">
-        <h1 id="city">{weatherData.city} </h1>
+        <ul className="current-weather-info-list">
+          <li>
+            Last update: <span id="date">{weatherData.date}</span>
+          </li>
+          <li id="description">{weatherData.description}</li>
+          <li>
+            Humidity: <span id="humidity">{weatherData.humidity}</span>%
+          </li>
+          <li>
+            Wind: <span id="wind">{weatherData.wind}</span> km/h
+          </li>
+        </ul>
+        <small>
+          <a href="https://github.com/seheichenko-anna/weather-react.git">
+            Open-source code
+          </a>
+        </small>
       </div>
-      <div className="current-weather">
-        <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
-          alt="weather icon"
-          id="icon"
-          width="100"
-        />
-        <p id="temperature">{weatherData.temperature}</p>
-        <div className="units">
-          <sup>°C</sup>
-        </div>
-      </div>
-      <ul className="current-weather-info-list">
-        <li>
-          Last update: <span id="date">{weatherData.date}</span>
-        </li>
-        <li id="description">Clouds</li>
-        <li>
-          Humidity: <span id="humidity">{weatherData.humidity}</span>%
-        </li>
-        <li>
-          Wind: <span id="wind">{weatherData.wind}</span> km/h
-        </li>
-      </ul>
-      <small>
-        <a href="https://github.com/seheichenko-anna/weather-react.git">
-          Open-source code
-        </a>
-      </small>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
